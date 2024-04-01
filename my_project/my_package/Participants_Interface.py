a76a183fa19907a3617118f4923d732d8deb15ae
Daniela Gallegos Dupuis <danigallegdup@gmail.com>
2024-03-31 17:28:08 -0700

    tried with little avail to seperate classes

---
+++ my_project/my_package/Participants_Interface.py
@@ -0,0 +1,92 @@
+import csv
+import threading
+import time
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from datetime import datetime
+
+from .Controller import Controller
+
+class Participants_Interface:
+    def __init__(self, root, data_dictionary, controller):
+       
+        self.controller = controller
+        self.root = root
+        self.keys_list = list(data_dictionary.keys())
+        self.data_dictionary = data_dictionary
+        self.state = -1  # 0: prompt, 1: table, 2: progress
+        self.can_progress = True
+        self.previous_counter = controller.get_counter()
+
+        self.initialize_gui()
+        self.bind_events()
+        self.update_screen()
+
+    def initialize_gui(self):
+        # GUI setup
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
+        self.photo = ImageTk.PhotoImage(Image.new('RGB', (self.screen_width, self.screen_height)))
+        self.picture_label = tk.Label(self.root, image=self.photo)
+        self.picture_label.pack(fill="both", expand=True)
+
+    def bind_events(self):
+        self.root.bind('<space>', self.update_screen)
+
+    def display_intro_text(self):
+        self.picture_label.config(text=Introduction, image='', font=("Helvetica", font_size), padx=10, pady=10)
+
+    def display_thank_you(self):
+        self.picture_label.config(text=Thank_You, image='', font=("Helvetica", font_size), padx=10, pady=10)
+
+    def display_prompt(self, task_information):
+        message = f"{task_information[Task_Prompt_Index]} \n{Prompt} "
+        self.picture_label.config(text=message, image='', font=("Helvetica", font_size), padx=10, pady=10)
+
+    def display_table(self, task_information):
+        self.controller.set_start_time_milliseconds(time.time() * 1000)
+        img = Image.open(task_information[Table_PNG_Index])
+        img = img.resize((self.screen_width, self.screen_height), Image.LANCZOS)
+        self.photo = ImageTk.PhotoImage(img)
+        self.picture_label.config(image=self.photo, text='')
+
+    def display_progress(self):
+        self.controller.set_end_time_milliseconds(time.time() * 1000)
+        progress_text = f"Completed {self.controller.get_counter()+1} out of 80 tasks" + "\n" + Progress
+        if(self.controller.get_counter()+1 % 16==0 and self.controller.get_counter() >1):
+            progress_text = progress_text + "\n\n" + Next_task
+        self.picture_label.config(text=progress_text, image='', font=("Helvetica", font_size), padx=10, pady=10)
+
+    def update_screen(self, event=None):
+        if self.previous_counter < self.controller.get_counter():
+            self.can_progress = True
+            self.state = 0
+            print("Counter updated, proccress and resume", self.state)
+        
+        if self.controller.get_counter() == 80:
+            self.display_thank_you()
+            return
+
+        name_of_task = self.keys_list[self.controller.get_counter()]
+        task_information = self.data_dictionary[name_of_task]
+
+        if self.state == -1:
+            self.display_intro_text()
+        elif self.state == 0:
+            self.display_prompt(task_information)
+        elif self.state == 1:
+            self.controller.start_mouse_logging()
+            self.display_table(task_information)
+        elif self.state == 2:
+            self.controller.stop_mouse_logging()
+            self.display_progress()
+            self.can_progress = False
+
+        if self.can_progress:
+            self.state = (self.state + 1) % 3
+
+        self.previous_counter = self.controller.get_counter()
+        if self.state == 0:
+            self.controller.update_counter()
+    
---
+++ my_project/start_experiment.py
@@ -0,0 +1,64 @@
+import csv
+import threading
+import time
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from datetime import datetime
+
+from my_package import Constants
+from my_package import Participants_Interface
+from my_package import Experimenters_Interface
+from my_package import Controller
+
+
+# Function to convert CSV file to a dictionary
+def csv_to_row_dict(csv_file_path):
+    """Converts a CSV file into a dictionary with custom keys."""
+    row_dict = {}
+    with open(csv_file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        headers = next(reader)  # Get the header row
+
+        for row in reader:
+            key = f"{row[headers.index('Topic')]}-{row[headers.index('Condition')]}-{row[headers.index('Task')]}-{row[headers.index('Repetition')]}"
+            row_dict[key] = row
+    return row_dict       
+
+def start_mainlog():
+    variable_names = ("Experiment_permutation", "participant_ID", "Task_iteration",
+                  "name_of_task", "Topic", "Condition", "Task", "Repetition",
+                  "table_png", "table_rendering", "start_time", "end_time", "start_time_e",
+                  "end_time_e", "start_time_t", "time_spent", "Recorded_Answer", "Prompt_col",
+                  "Recorded_row_col", "Expected_Answer", "Expected_Answer_row_col", 
+                  "error", "isPerfect", "Is_correct")
+
+    with open(Constants.OutputFilePaths.mainlog_file, 'w', newline='') as f:
+        csv.writer(f).writerow(variable_names)
+
+def both_screen(data_dictionary):
+    start_mainlog()
+    controller = Controller()
+
+    root = tk.Tk()
+    root.geometry("1700x900")
+    Experimenters_Interface(root, data_dictionary, controller)
+
+    top = tk.Tk()
+    top = tk.Toplevel(root)
+    top.attributes("-fullscreen", True)
+    Participants_Interface(top, data_dictionary, controller)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    Experiment_Permutation = int(input("Input the experiment permutation: "))
+    Participant_ID = input("Input the participant ID: ")
+
+    # Experiment_Results\E1_A\ExperimentPermuation1_ParticipantA_Input.csv
+
+    Input_File_Path = f'./Experiment_Results/Experiment_permutation_{Experiment_Permutation}_participant_{Participant_ID}/ExperimentPermuation{Experiment_Permutation}_Participant{Participant_ID}_Input.csv'
+
+    data_dictionary = csv_to_row_dict(Input_File_Path)  # Convert CSV to dictionary
+    both_screen(data_dictionary)
+   
